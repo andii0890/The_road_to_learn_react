590cd65bdb8874c430fb4cdc3d5103e846792aa3
Andrea <greenlounge@hotmail.com>
2019-11-07 15:29:59 -0600

    Introduction to JSX,ES6 const and let, ReactDOM, HRM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io.react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url:'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render(){
     const helloWord = 'Welcome to the Road to learn React'
     return (
       <div className="App">
-        <h2>{helloWord}</h2>
+       {list.map(function(item) {
+         return <div>{item.title}</div>
+       })}
       </div>
     );
   }
